436761444b094212092ee8d05abb2ed533ab0cc9
Nick Klose <nick.klose@ualberta.ca>
2013-03-11 01:33:55 -0600

    Implemented listwidget for set of phoneme sets.

--- Customization/customization.py
+++ Customization/customization.py
@@ -15,6 +15,7 @@ import sys
 from PyQt4 import QtCore, QtGui
 from customization_gui import Ui_mainWindow
 from os.path import basename
+from phoneme_set import *
 
 class StartQT4(QtGui.QMainWindow):
     def __init__(self, parent = None):
@@ -22,9 +23,22 @@ class StartQT4(QtGui.QMainWindow):
         self.ui = Ui_mainWindow()
         self.ui.setupUi(self)
 
+        # Make settings frame invisible until a set is selected
+        self.ui.frame.setVisible(0)
+
+        # Instantiate the set of phoneme sets, and add the first
+        # one to the list of sets.
+        self.sets = [Phoneme_Set()]
+        self.update_sets()
+
         #######################################################
         # Interface Object Connections                        #
         #######################################################
+
+        ## List Widget
+        QtCore.QObject.connect(self.ui.listSets,
+                               QtCore.SIGNAL("itemClicked(QListWidgetItem *)"),
+                               self.select_item)
 
         ## Buttons
 		# Load Goal Image
@@ -37,6 +51,39 @@ class StartQT4(QtGui.QMainWindow):
         path = str(QtGui.QFileDialog.getOpenFileName())
         file = basename(path)
         self.ui.imgGoal.setPixmap(QtGui.QPixmap(path))
+
+    # Updates the displayed list of sets with the internal list.
+    def update_sets(self):
+        self.ui.listSets.clear()
+        index = 1
+        for item in self.sets:
+            self.ui.listSets.addItem("Set " + str(index))
+            index += 1
+
+    # This function is called when the user selects a specific item
+    # from the list of sets.
+    def select_item(self, item):
+        # show frame if it isn't there already
+        self.ui.frame.setVisible(1)
+
+        # get the index of the selected item
+        index = self.index_from_item(item)
+
+        # load the goal for the selected item
+        #self.ui.lblGoalImagePath.text(self.sets[index].goal.image_path)
+        goal_image_path = self.sets[index].goal.image_path
+        self.ui.lblGoalImagePath.setText(goal_image_path)
+        goal_sound_path = self.sets[index].goal.sound_path
+        self.ui.lblGoalSoundPath.setText(goal_sound_path)
+        
+        # load the image, if available
+        if goal_image_path != "<none>":
+            self.ui.imgGoal.setPixmap(QtGui.QPixmap(goal_image_path))
+        
+
+    # Returns the index if a given item as an integer.
+    def index_from_item(self, item):
+        return int(item.text()[4:]) - 1
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
