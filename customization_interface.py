3739b458cb2d8dd0f10ed6730a72487fde9dad71
Nick Klose <nick.klose@ualberta.ca>
2013-03-10 23:56:14 -0600

    Converted main.ui to .py file.

---
+++ customization_interface.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main.ui'
+#
+# Created: Sun Mar 10 23:55:07 2013
+#      by: PyQt4 UI code generator 4.9.3
+#
+# WARNING! All changes made in this file will be lost!
+
