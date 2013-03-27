f9068e128f11a578a3a968b7b84faf768b46c0b4
Andrew Neufeld <adneufel@ualberta.ca>
2013-03-27 12:14:23 -0600

    Completed backend customization code
    - Customization of assets.lua and levels.gen.cpp is completed.
    - Visually verified as correct. No actual testing done.
    - Not integrated into the UI as of yet.

---
+++ Customization/assets.lua
@@ -0,0 +1,112 @@
+-- Metadata
+IconAssets = group{quality=9.95}
+Icon = image{"assets/icon.png"}
+
+-- Asset Group loaded initially to all cubes
+BootAssets = group{quality=8.90}
+LoadingBg = image{"assets/loading.png"}
+Bravo = image{"assets/bravo.png"}
+Title = image{"assets/welcomeTitle.png"}
+Grid = image{"assets/playfield.png", pinned=1}
+Speaker = image{"assets/speaker.png"}
+
+-- Menu Assets
+MenuAssetGrp = group{quality=9.05}
+BgTile = image{ "assets/Menu/bg.png", pinned=1 }
+Grid = image{"assets/playfield.png", pinned=1}
+IconSandwich = image{ "assets/Menu/IconSandwich.png" }
+IconChroma = image{ "assets/Menu/IconChroma.png" }
+Tip0 = image{ "assets/Menu/Tip0.png" }
+Tip1 = image{ "assets/Menu/Tip1.png" }
+Footer = image{ "assets/Menu/Footer.png" }
+LabelEmpty = image{ "assets/Menu/LabelEmpty.png" }
+LabelUser1 = image{"assets/Menu/LabelAdrian.png"}
+LabelUser2 = image{"assets/Menu/LabelMatthew.png"}
+
+-- Level1 asset group
+Level1Assets = group{quality=10}
+L1Phoneme1 = image{"full/image_path1.file"}
+L1Sound1 = sound{"full/sound_path1.file", encode="pcm"}
+L1Phoneme2 = image{"full/image_path2.file"}
+L1Sound2 = sound{"full/sound_path2.file", encode="pcm"}
+L1Phoneme3 = image{"full/image_path3.file"}
+L1Sound3 = sound{"full/sound_path3.file", encode="pcm"}
+L1GoalSound = sound{"full/sound_path1.file", encode="pcm"}
+
+
+-- Level2 asset group
+Level2Assets = group{quality=10}
+L2Phoneme1 = image{"full/image_path1.file"}
+L2Sound1 = sound{"full/sound_path1.file", encode="pcm"}
+L2Phoneme2 = image{"full/image_path2.file"}
+L2Sound2 = sound{"full/sound_path2.file", encode="pcm"}
+L2Phoneme3 = image{"full/image_path3.file"}
+L2Sound3 = sound{"full/sound_path3.file", encode="pcm"}
+L2GoalSound = sound{"full/sound_path1.file", encode="pcm"}
+
+
+-- Level3 asset group
+Level3Assets = group{quality=10}
+L3Phoneme1 = image{"full/image_path1.file"}
+L3Sound1 = sound{"full/sound_path1.file", encode="pcm"}
+L3Phoneme2 = image{"full/image_path2.file"}
+L3Sound2 = sound{"full/sound_path2.file", encode="pcm"}
+L3Phoneme3 = image{"full/image_path3.file"}
+L3Sound3 = sound{"full/sound_path3.file", encode="pcm"}
+L3GoalSound = sound{"full/sound_path1.file", encode="pcm"}
+
+
+-- Level4 asset group
+Level4Assets = group{quality=10}
+L4Phoneme1 = image{"full/image_path1.file"}
+L4Sound1 = sound{"full/sound_path1.file", encode="pcm"}
+L4Phoneme2 = image{"full/image_path2.file"}
+L4Sound2 = sound{"full/sound_path2.file", encode="pcm"}
+L4Phoneme3 = image{"full/image_path3.file"}
+L4Sound3 = sound{"full/sound_path3.file", encode="pcm"}
+L4GoalSound = sound{"full/sound_path1.file", encode="pcm"}
+
+
+-- Level5 asset group
+Level5Assets = group{quality=10}
+L5Phoneme1 = image{"full/image_path1.file"}
+L5Sound1 = sound{"full/sound_path1.file", encode="pcm"}
+L5Phoneme2 = image{"full/image_path2.file"}
+L5Sound2 = sound{"full/sound_path2.file", encode="pcm"}
+L5Phoneme3 = image{"full/image_path3.file"}
+L5Sound3 = sound{"full/sound_path3.file", encode="pcm"}
+L5GoalSound = sound{"full/sound_path1.file", encode="pcm"}
+
+
+-- Level6 asset group
+Level6Assets = group{quality=10}
+L6Phoneme1 = image{"full/image_path1.file"}
+L6Sound1 = sound{"full/sound_path1.file", encode="pcm"}
+L6Phoneme2 = image{"full/image_path2.file"}
+L6Sound2 = sound{"full/sound_path2.file", encode="pcm"}
+L6Phoneme3 = image{"full/image_path3.file"}
+L6Sound3 = sound{"full/sound_path3.file", encode="pcm"}
+L6GoalSound = sound{"full/sound_path1.file", encode="pcm"}
+
+
+-- Level7 asset group
+Level7Assets = group{quality=10}
+L7Phoneme1 = image{"full/image_path1.file"}
+L7Sound1 = sound{"full/sound_path1.file", encode="pcm"}
+L7Phoneme2 = image{"full/image_path2.file"}
+L7Sound2 = sound{"full/sound_path2.file", encode="pcm"}
+L7Phoneme3 = image{"full/image_path3.file"}
+L7Sound3 = sound{"full/sound_path3.file", encode="pcm"}
+L7GoalSound = sound{"full/sound_path1.file", encode="pcm"}
+
+
+-- Level8 asset group
+Level8Assets = group{quality=10}
+L8Phoneme1 = image{"full/image_path1.file"}
+L8Sound1 = sound{"full/sound_path1.file", encode="pcm"}
+L8Phoneme2 = image{"full/image_path2.file"}
+L8Sound2 = sound{"full/sound_path2.file", encode="pcm"}
+L8Phoneme3 = image{"full/image_path3.file"}
+L8Sound3 = sound{"full/sound_path3.file", encode="pcm"}
+L8GoalSound = sound{"full/sound_path1.file", encode="pcm"}
+
--- Customization/file_customizer.py
+++ Customization/file_customizer.py
@@ -5,12 +5,33 @@ Some basic constant values
 """
 # Static string for includes of levels.gen.cpp
 LEVELS_GEN_TOP = '#include <sifteo.h>\n#include "levels.gen.h"\n#include "assets.gen.h"\n'
+# Static string that contains the static assets within assets.lua
+ASSETS_LUA_TOP = (
+		'-- Metadata\nIconAssets = group{quality=9.95}\nIcon = image{"assets/icon.png"}\n\n'
+		'-- Asset Group loaded initially to all cubes\nBootAssets = group{quality=8.90}\n'
+		'LoadingBg = image{"assets/loading.png"}\nBravo = image{"assets/bravo.png"}\n'
+		'Title = image{"assets/welcomeTitle.png"}\nGrid = image{"assets/playfield.png", pinned=1}\n'
+		'Speaker = image{"assets/speaker.png"}\n\n-- Menu Assets\nMenuAssetGrp = group{quality=9.05}\n'
+		'BgTile = image{ "assets/Menu/bg.png", pinned=1 }\nGrid = image{"assets/playfield.png", pinned=1}\n'
+		'IconSandwich = image{ "assets/Menu/IconSandwich.png" }\nIconChroma = image{ "assets/Menu/IconChroma.png" }\n'
+		'Tip0 = image{ "assets/Menu/Tip0.png" }\nTip1 = image{ "assets/Menu/Tip1.png" }\n'
+		'Footer = image{ "assets/Menu/Footer.png" }\nLabelEmpty = image{ "assets/Menu/LabelEmpty.png" }\n'
+		'LabelUser1 = image{"assets/Menu/LabelAdrian.png"}\nLabelUser2 = image{"assets/Menu/LabelMatthew.png"}\n'
+	)
 # Number of images per set
 NUM_IN_SET = 3
 
-def generate_files(levels):
-	append_assetsLua(levels)
-	generate_levelGenCpp(levels)
+"""
+	generate_files(levels, path): 	- levels is a list of Level() objects containing game data
+									- path is a string with a file path to the target directory
+											for the file creation
+
+	This function will create the "assets.lua" file and the "levels.gen.cpp" generate_files
+		that are used for the custom game data of the Sifteos Phoneme Awareness game
+"""
+def generate_files(levels, path):
+	append_assetsLua(levels, path)
+	generate_levelGenCpp(levels, path)
 
 def append_assetsLua(levels):
 	# get the overall number of LevelSets
@@ -22,13 +43,16 @@ def append_assetsLua(levels):
 	# keep track of the overall LevelSet num
 	levelNum = 0
 
-	assetsLua = open('assets.lua', mode='a')		# append mode will append, not replace
-	for i in range(0, numLevels)
+	assetsLua = open(path + 'assets.lua', mode='w')		# write mode will replace any existing file
+	assetsLua.write(ASSETS_LUA_TOP)
+
+	# write levels
+	for i in range(0, numLevels):
 		level = levels[x]
-		for j in range(0, len(level.sets))
+		for j in range(0, len(level.sets)):
 			levelNum += 1
 			set = level.sets[j]
-			append_LevelAssets(assetsLua, set, levelNum)	
+			append_levelAssets(assetsLua, set, levelNum)	
 
 	# and remember to close the file
 	assetsLua.close()
@@ -49,7 +73,7 @@ def append_levelAssets(assetfile, set, num):
 
 		# if goal phoneme then get filepath for GoalSound write below
 		if phoneme.goal == True:
-			goalFilepath = soundFilepath	
+			goalFilepath = soundFilepath
 
 		assetfile.write('L' + str(num) + 'Phoneme' + str(x) + ' = image{"' + imgFilepath + '"}\n')
 		assetfile.write('L' + str(num) + 'Sound' + str(x) + ' = sound{"' + soundFilepath + '", encode="pcm"}\n')
@@ -57,17 +81,15 @@ def append_levelAssets(assetfile, set, num):
 	# write goal sound 
 	assetfile.write('L' + str(num) + 'GoalSound = sound{"' + goalFilepath + '", encode="pcm"}\n\n')
 
-
 def generate_levelGenCpp(levels):
 	# get the overall number of LevelSets
-	numLevels = len(levels)
-"""
 	numLevels = 0
 	for x in range(0, len(levels)):
 		level = levels[x]
 		numLevels += len(level.sets)
-"""
-	levelsGenCpp = open('levels.gen.cpp', mode='w') 	# write mode will replace entire file each time
+
+	# open the file!
+	levelsGenCpp = open(path + 'levels.gen.cpp', mode='w') 	# write mode will replace entire file each time
 
 	# Write the begining, constant portion with includes to the file
 	levelsGenCpp.write(LEVELS_GEN_TOP + '\n')
@@ -103,14 +125,14 @@ def create_level_line(num):
 	line = "struct Level Level" + str(num) + " = { "
 	
 	# add the static initializer for Level.phonemes[] "{L1Phoneme1, L1Phoneme2, etc" etc
- 	line += '{'
+	line += '{'
 	for x in range (1, NUM_IN_SET+1):
 		line += create_asset_token(num, x, 'Phoneme')
 		line += ', '
 	line += '}, '
 
 	# add the static initializer for Level.sounds[] "{L1Sound, L1Sound, etc" etc
- 	line += '{'
+	line += '{'
 	for x in range (1, NUM_IN_SET+1):
 		line += create_asset_token(num, x, 'Sound')
 		if x < num:
@@ -125,5 +147,24 @@ def create_level_line(num):
 def create_asset_token(levelNum, phonemeNum, assetType):
 	return "L" + str(levelNum) + assetType + str(phonemeNum)
 
-if __name__ == "__main__":
-	generate_levelGenCpp([[1,2,3], [1,2,3],[1,2,3]])
+if __name__ == "__main__":	
+	levels = []
+	for x in range(0, 2):
+		levels.append(Level())
+		for y in range(0,2):
+			level = levels[x]
+			level.sets.append(Set())
+			sett = level.sets[y]
+			for z in range(0,3):
+				ph = Phoneme()
+				ph.name = "Test"
+				ph.text = "testtxt"
+				ph.image_path = "full/image_path" + str(z+1) + ".file"
+				ph.sound_path = "full/sound_path" + str(z+1) + ".file"
+				if z == 0:
+					ph.goal = True
+				else:
+					ph.goal = False
+				sett.add_phoneme(ph)
+
+	generate_files(levels, "")
