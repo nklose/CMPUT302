e1d931c7efc266e9ac08ae482f2e8d304188ea63
adneufel <andrew.neuf@gmail.com>
2013-04-08 13:33:25 -0600

    Update assets.lua

--- PhonemeFrenzy/assets.lua
+++ PhonemeFrenzy/assets.lua
@@ -11,5 +11,183 @@ Grid = image{"assets/playfield.png"}
 Speaker = image{"assets/speaker.png"}
 
 
--- Level1 asset group
+-- Level1 (Format for 1 level with 3 images in this file)
+
 Level1Assets = group{quality=10}
+
+L1Phoneme1 = image{"assets/Level1/bain.png"}
+
+L1Phoneme2 = image{"assets/Level1/daim.png"}
+
+L1Phoneme3 = image{"assets/Level1/vin.png"}
+
+L1GoalSound = sound{"assets/Level1/goalsound.wav", encode="pcm" }
+
+L1Sound1 = sound{"assets/Level1/b.wav", encode="pcm" }
+
+L1Sound2 = sound{"assets/Level1/d.wav", encode="pcm" }
+
+L1Sound3 = sound{"assets/Level1/v.wav", encode="pcm" }
+Title = image{"assets/title.png"}
+
+
+-- Level2
+Level2Assets = group{quality=10}
+
+L2Phoneme1 = image{"assets/Level2/fait.png"}
+
+L2Phoneme2 = image{"assets/Level2/paix.png"}
+
+L2Phoneme3 = image{"assets/Level2/taie.png"}
+
+L2GoalSound = sound{"assets/Level2/goalsound.wav", encode="pcm" }
+
+L2Sound1 = sound{"assets/Level2/f.wav", encode="pcm" }
+
+L2Sound2 = sound{"assets/Level2/p.wav", encode="pcm" }
+
+L2Sound3 = sound{"assets/Level2/t.wav", encode="pcm" }
+
+
+-- Level3
+Level3Assets = group{quality=10}
+
+L3Phoneme1 = image{"assets/Level3/gaie.png"}
+
+L3Phoneme2 = image{"assets/Level3/raie.png"}
+
+L3Phoneme3 = image{"assets/Level3/sait.png"}
+
+L3GoalSound = sound{"assets/Level3/goalsound.wav", encode="pcm" }
+
+L3Sound1 = sound{"assets/Level3/g.wav", encode="pcm" }
+
+L3Sound2 = sound{"assets/Level3/r.wav", encode="pcm" }
+
+L3Sound3 = sound{"assets/Level3/s.wav", encode="pcm" }
+
+
+-- Level4
+Level4Assets = group{quality=10}
+
+L4Phoneme1 = image{"assets/Level4/lin.png"}
+
+L4Phoneme2 = image{"assets/Level4/main.png"}
+
+L4Phoneme3 = image{"assets/Level4/saint.png"}
+
+L4GoalSound = sound{"assets/Level4/goalsound.wav", encode="pcm" }
+
+L4Sound1 = sound{"assets/Level4/l.wav", encode="pcm" }
+
+L4Sound2 = sound{"assets/Level4/m.wav", encode="pcm" }
+
+L4Sound3 = sound{"assets/Level4/s.wav", encode="pcm" }
+
+
+-- Level5
+Level5Assets = group{quality=10}
+
+L5Phoneme1 = image{"assets/Level5/daim.png"}
+
+L5Phoneme2 = image{"assets/Level5/gaie.png"}
+
+L5Phoneme3 = image{"assets/Level5/taie.png"}
+
+L5GoalSound = sound{"assets/Level5/goalsound.wav", encode="pcm" }
+
+L5Sound1 = sound{"assets/Level5/d.wav", encode="pcm" }
+
+L5Sound2 = sound{"assets/Level5/g.wav", encode="pcm" }
+
+L5Sound3 = sound{"assets/Level5/t.wav", encode="pcm" }
+
+
+-- Level6
+Level6Assets = group{quality=10}
+
+L6Phoneme1 = image{"assets/Level6/bain.png"}
+
+L6Phoneme2 = image{"assets/Level6/lin.png"}
+
+L6Phoneme3 = image{"assets/Level6/sait.png"}
+
+L6GoalSound = sound{"assets/Level6/goalsound.wav", encode="pcm" }
+
+L6Sound1 = sound{"assets/Level6/b.wav", encode="pcm" }
+
+L6Sound2 = sound{"assets/Level6/l.wav", encode="pcm" }
+
+L6Sound3 = sound{"assets/Level6/s.wav", encode="pcm" }
+
+
+-- Level7
+Level7Assets = group{quality=10}
+
+L7Phoneme1 = image{"assets/Level7/saint.png"}
+
+L7Phoneme2 = image{"assets/Level7/taie.png"}
+
+L7Phoneme3 = image{"assets/Level7/vin.png"}
+
+L7GoalSound = sound{"assets/Level7/goalsound.wav", encode="pcm" }
+
+L7Sound1 = sound{"assets/Level7/s.wav", encode="pcm" }
+
+L7Sound2 = sound{"assets/Level7/t.wav", encode="pcm" }
+
+L7Sound3 = sound{"assets/Level7/v.wav", encode="pcm" }
+
+
+-- Level8
+Level8Assets = group{quality=10}
+
+L8Phoneme1 = image{"assets/Level8/fait.png"}
+
+L8Phoneme2 = image{"assets/Level8/main.png"}
+
+L8Phoneme3 = image{"assets/Level8/saint.png"}
+
+L8GoalSound = sound{"assets/Level8/goalsound.wav", encode="pcm" }
+
+L8Sound1 = sound{"assets/Level8/f.wav", encode="pcm" }
+
+L8Sound2 = sound{"assets/Level8/m.wav", encode="pcm" }
+
+L8Sound3 = sound{"assets/Level8/s.wav", encode="pcm" }
+
+
+-- Level9
+Level9Assets = group{quality=10}
+
+L9Phoneme1 = image{"assets/Level9/bain.png"}
+
+L9Phoneme2 = image{"assets/Level9/daim.png"}
+
+L9Phoneme3 = image{"assets/Level9/taie.png"}
+
+L9GoalSound = sound{"assets/Level9/goalsound.wav", encode="pcm" }
+
+L9Sound1 = sound{"assets/Level9/b.wav", encode="pcm" }
+
+L9Sound2 = sound{"assets/Level9/d.wav", encode="pcm" }
+
+L9Sound3 = sound{"assets/Level9/t.wav", encode="pcm" }
+
+
+-- Level10
+Level10Assets = group{quality=10}
+
+L10Phoneme1 = image{"assets/Level10/daim.png"}
+
+L10Phoneme2 = image{"assets/Level10/paix.png"}
+
+L10Phoneme3 = image{"assets/Level10/raie.png"}
+
+L10GoalSound = sound{"assets/Level10/goalsound.wav", encode="pcm" }
+
+L10Sound1 = sound{"assets/Level10/d.wav", encode="pcm" }
+
+L10Sound2 = sound{"assets/Level10/p.wav", encode="pcm" }
+
+L10Sound3 = sound{"assets/Level10/r.wav", encode="pcm" }
